3d3ce89328be5855042eea1487cd792ac8891891
Chris Belyea <chris@chrisbelyea.com>
2018-05-19 09:39:22 -0400

    Add implemented sponsored suggestion function

--- suggestion.js
+++ suggestion.js
@@ -1,10 +1,41 @@
+var document = "";
+
 function printSuggestion(inputData) {
-  var suggestion = inputData[Math.floor(Math.random() * inputData.length)];
+  console.group("Generating a suggestion");
+  console.debug("Received input data: \n" + JSON.stringify(inputData, null, '  '));
+  var sponsoredData = [];
+  // iterate over array of suggestions and add sponsored suggestions to a new
+  // sponsored array
+  console.group("Checking for sponsored suggestions");
+  for (var i = 0; i < inputData.length; i++) {
+    console.debug("Evaluating: " + inputData[i].name);
+    console.debug("  Sponsored: " + inputData[i].sponsored);
+    if (inputData[i].sponsored === true) {
+      sponsoredData.push(inputData[i]);
+    }
+  }
+  console.debug("Sponsored suggestions: \n" + JSON.stringify(sponsoredData, null, '  '));
+  console.groupEnd();
+
+  // combine all input data with sponsored data; meaning sponsored posts are
+  // twice as likely to be shown
+  var suggestions = inputData.concat(sponsoredData);
+  console.debug("Processed suggestions data: \n" + JSON.stringify(suggestions, null, '  '));
+
+  // get a random suggestion and update the page with it
+  var suggestion = suggestions[Math.floor(Math.random() * suggestions.length)];
   console.group("Suggestion");
   console.log(JSON.stringify(suggestion, null, '  '));
   console.groupEnd();
   document.getElementById("suggestion").innerHTML =
-    '<div class="fyi">' +
-    '<p>The updated printSuggestion() function that supports sponsored ' +
-    'suggestions is still in development.</p></div>';
+    '<div class="vcard">' +
+      '<div class="org">' + suggestion.name + '</div>' +
+      '<div class="adr">' +
+        '<div class="street-address">' + suggestion.address + '</div>' +
+        '<span class="locality">Richmond</span>, ' +
+        '<span class="state">VA</span>' +
+      '</div>' +
+      '<div class="note">' + suggestion.note + '</div>' +
+    '</div>';
+  console.groupEnd();
 }
